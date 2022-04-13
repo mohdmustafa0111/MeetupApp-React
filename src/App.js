3feead3cbb4b1660fa3ec2c080f37828d6b68309
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2022-04-14 01:36:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AllMeetups from "./pages/AllMeetups";
+import NewMeetups from "./pages/NewMeetups";
+import Favourite from "./pages/Favourite";
+import Layout from "./components/layouts/Layout";
+import { FavoritesContextProvider } from "./store/favorites-context";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FavoritesContextProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<AllMeetups />} />
+            <Route path="/new-meetup" element={<NewMeetups />} />
+            <Route path="/favorites" element={<Favourite />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </FavoritesContextProvider>
   );
 }
 
---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+function AllMeetups() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    fetch("https://react-meetup-1d99a-default-rtdb.firebaseio.com/meetups.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetup</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
+}
+
+export default AllMeetups;
